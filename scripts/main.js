542bf99f4dd7b39c393cdc3ffd933f4288ac67e4
mi-os <jbhjmy@yahoo.co.jp>
2020-08-13 15:44:13 +0900

    del 3 methods

--- scripts/main.js
+++ scripts/main.js
@@ -24,28 +24,7 @@ class Main {
         this._scrollInit();
     }
 
-    // _sideAnimation(el, inview) {
-    //     if(inview) {
-    //         this.sides.forEach(side => side.classList.add('inview'));
-    //     } else {
-    //         this.sides.forEach(side => side.classList.remove('inview'));
-    //     }
-    // }
-
-    // _destroyObservers() {
-    //     this.observers.forEach(ob => {
-    //         ob.destroy();
-    //     });
-    // }
-
-    // destroy() {
-    //     this._destroyObservers();
-    // }
-
     _scrollInit() {
         console.log("This is makoto's portfolio site!");
-        // this.observers = new ScrollObserver('.appear', this._inviewAnimation);
-        // this.observers = new ScrollObserver('.tween-animate-title', this._textAnimation, {rootMargin: "-200px 0px"});
-        // this.observers = new ScrollObserver('#main-content', this._sideAnimation.bind(this), {once: false, rootMargin: "-300px 0px"});
     }
 }
